e7efce4109a4f98dbe0ab23318cd2004959f8060
marina2144ns <131006638+marina2144ns@users.noreply.github.com>
2026-08-25 18:11:07 +0200

    Rename information card DDL by domain

---
+++ sql/ddl/information_cards.sql
@@ -0,0 +1,48 @@
+USE [BonusStorage];
+GO
+
+SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+CREATE TABLE [dbo].[InformationCards]
+(
+    [Id] BIGINT IDENTITY(1,1) NOT NULL,
+    [CardId] BINARY(16) NOT NULL,
+    [CardCode] NVARCHAR(200) NULL,
+    [PersonId] BINARY(16) NULL,
+    [CurrentVersion] BIGINT NOT NULL,
+
+    CONSTRAINT [PK_InformationCards]
+        PRIMARY KEY CLUSTERED ([Id])
+);
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UX_InformationCards_CardId]
+    ON [dbo].[InformationCards] ([CardId]);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_InformationCards_CardCode]
+    ON [dbo].[InformationCards] ([CardCode]);
+GO
+
+CREATE TABLE [dbo].[PersonPhones]
+(
+    [Id] BIGINT IDENTITY(1,1) NOT NULL,
+    [PersonId] BINARY(16) NOT NULL,
+    [Phone] NVARCHAR(100) NOT NULL,
+    [CurrentVersion] BIGINT NOT NULL,
+
+    CONSTRAINT [PK_PersonPhones]
+        PRIMARY KEY CLUSTERED ([Id])
+);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_PersonPhones_PersonId]
+    ON [dbo].[PersonPhones] ([PersonId]);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_PersonPhones_Phone]
+    ON [dbo].[PersonPhones] ([Phone]);
+GO
